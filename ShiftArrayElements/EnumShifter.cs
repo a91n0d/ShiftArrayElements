392d5cfbd715ea862109de588c948e715a8d1659
a91n0d <a91n0d@gmail.com>
2022-01-12 09:33:05 +0300

    Add project files.

---
+++ ShiftArrayElements/EnumShifter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class EnumShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using directions from <see cref="directions"/> array, one element shift per each direction array element.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="directions">An array with directions.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">directions array is null.</exception>
+        /// <exception cref="InvalidOperationException">direction array contains an element that is not <see cref="Direction.Left"/> or <see cref="Direction.Right"/>.</exception>
+        public static int[] Shift(int[] source, Direction[] directions)
+        {
+            if (directions is null)
+            {
+                throw new ArgumentNullException(nameof(directions));
+            }
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                switch (directions[i])
+                {
+                    case Direction.Left:
+                        {
+                            source = source.LeftShift();
+                            break;
+                        }
+
+                    case Direction.Right:
+                        {
+                            source = source.RightShift();
+                            break;
+                        }
+
+                    default:
+                        throw new InvalidOperationException($"Incorrect {directions[i]} enum value.");
+                }
+            }
+
+            return source;
+        }
+
+        /// <summary>
+        /// Shifts elements array to left.
+        /// </summary>
+        /// <param name="source">>A source array.</param>
+        /// <returns>An array with shifted to left elements.</returns>
+        public static int[] LeftShift(this int[] source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            int[] shiftArray = new int[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (i < source.Length - 1)
+                {
+                    shiftArray[i] = source[i + 1];
+                }
+                else
+                {
+                    shiftArray[i] = source[0];
+                }
+            }
+
+            return shiftArray;
+        }
+
+        /// <summary>
+        /// Shifts elements array to right.
+        /// </summary>
+        /// <param name="source">>A source array.</param>
+        /// <returns>An array with shifted to right elements.</returns>
+        public static int[] RightShift(this int[] source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            int[] shiftArray = new int[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (i != 0)
+                {
+                    shiftArray[i] = source[i - 1];
+                }
+                else
+                {
+                    shiftArray[i] = source[^1];
+                }
+            }
+
+            return shiftArray;
+        }
+    }
+}
---
+++ ShiftArrayElements/Shifter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class Shifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using <see cref="iterations"/> array for getting directions and iterations (see README.md for detailed instructions).
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="iterations">An array with iterations.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">iterations array is null.</exception>
+        public static int[] Shift(int[] source, int[] iterations)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (iterations is null)
+            {
+                throw new ArgumentNullException(nameof(iterations));
+            }
+
+            for (int i = 0; i < iterations.Length; i++)
+            {
+                if (iterations[i] == 0)
+                {
+                    continue;
+                }
+
+                int direction = i % 2;
+                for (int n = iterations[i]; n != 0; n--)
+                {
+                    int temp = source[(source.Length - direction) % source.Length];
+                    Array.Copy(source, 1 - direction, source, direction, source.Length - 1);
+                    source[(source.Length + direction - 1) % source.Length] = temp;
+                }
+            }
+
+            return source;
+        }
+    }
+}
